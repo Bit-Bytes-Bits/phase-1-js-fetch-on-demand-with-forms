de03cb5433911c63494f6b0507bb2dcaa49c1563
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-08-28 06:34:01 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    //Accessing the user input:
+    // 1. Access value from event object
+    // console.log(event.target.children[1].value)
+    //2. Access input value directly
+    const inputValue = document.querySelector("input#searchByID");
+    // console.log(inputValue.value);
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${inputValue.value}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        // console.log(data);
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
